5087be8b2692f87636fcde5d2c7dd4c2c6e9c71c
hangphamhnue-lgtm <hangpham.hnue@gmail.com>
2026-08-20 20:52:56 +0700

    Update app-config.js

--- app-config.js
+++ app-config.js
@@ -2,17 +2,17 @@
 
 // Tệp sinh cục bộ; không chứa Gmail rõ, token hay client secret.
 window.TPT_APP_CONFIG = Object.freeze({
-  "APP_VERSION": "3.2.0",
-  "BUILD_ID": "20260815-smart-import-v10",
+  "APP_VERSION": "3.1.0-rc.1",
+  "BUILD_ID": "20260814-drive-rc1",
   "SCHEMA_VERSION": 10,
-  "APP_ID": "vn.giaoducso40.tpt.thcs.thcs",
-  "SCHOOL_PROFILE_ID": "thcs-thcs-profile-001",
-  "APP_NAMESPACE": "tpt_thcs_thcs_profile_001",
-  "DB_NAME": "TPT_DOI_THCS_THCS_DB",
-  "SCHOOL_NAME": "THCS NGUYỄN THỊ ĐỊNH",
+  "APP_ID": "vn.giaoducso40.tpt.thcs.th_thcs",
+  "SCHOOL_PROFILE_ID": "thcs-th-thcs-profile-001",
+  "APP_NAMESPACE": "tpt_thcs_th_thcs_profile_001",
+  "DB_NAME": "TPT_DOI_THCS_TH_THCS_DB",
+  "SCHOOL_NAME": "TH&THCS Trần Mai Ninh",
   "SCHOOL_LEVEL": "THCS",
   "GOOGLE_CLIENT_ID": "422851861968-emastosfnml2m0d8pp38m4hoovh2ihu3.apps.googleusercontent.com",
-  "EXPECTED_GOOGLE_EMAIL_SHA256": "3bd4a97daab011dc261cfdb42ede08779f2298d849821a758c0aa9ee1c3c413b",
+  "EXPECTED_GOOGLE_EMAIL_SHA256": "5604bc784265088f322eacdf1a2dff5ddae927ad4e509c6639c7a21ea9c47088",
   "EXPECTED_DRIVE_PERMISSION_ID": "",
   "REPOSITORY_NAME": "TROLYTONGPHUTRACH",
   "BASE_PATH": "./"
